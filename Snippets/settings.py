2345a76ce2b67fe9cba48b37eafb5979437aed20
FanJekkk <47693218+FanJekkk@users.noreply.github.com>
2022-09-14 12:58:50 +0300

    edit requirements.txt

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import django
+from django.utils.encoding import smart_str
+django.utils.encoding.smart_text = smart_str
 db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
